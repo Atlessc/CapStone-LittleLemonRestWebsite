740527c7332c0c1280a8ddadabea2dfe1b2d9d34
Tyler Smith <tylersmith4596@gmail.com>
2024-02-12 23:23:59 -0800

    Update styles and add container to homepage

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import '../../styles/pages/HomePage.css';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <h1>Welcome to Little Lemon</h1>
-      <p>A taste of Chicago's finest cuisine</p>
-    </div>
+    <div className="hero Conatiner">
+        <div className='word-area'>
+          <h1>Welcome to Little Lemon</h1>
+          <h3>Chicago's finest cuisine</h3>
+          <p>A taste of Chicago's finest cuisine</p>
+          <div></div>
+          <div></div>
+          <div></div>
+          <Link to="/reservations" className='btn'>
+            <button>
+              Reserve a table
+            </button>
+          </Link>
+        </div>
+        <img src="/images/restauranfood.jpg" alt="Chicago-style pizza" className='hero-img' />
+      </div>
   );
 };
 
